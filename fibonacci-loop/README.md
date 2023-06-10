007f36d3dc5b4175f631989d2ed3432f13900f4e
Tolga <109863384+TolgaYuceel@users.noreply.github.com>
2023-06-11 00:25:44 +0300

    fibonacci loop

---
+++ fibonacci-loop/README.md
@@ -0,0 +1,6 @@
+# Fibonacci Loop Problem
+- PROBLEM
+Write a function that calculates the first 'n' terms of the Fibonacci sequence for a given number 
+'n' and returns the result as an array. The Fibonacci sequence is a sequence where each number is the sum of 
+the two preceding numbers. For example, the first 10 terms of the 
+Fibonacci sequence are: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34].
---
+++ fibonacci-loop/fibonacci.js
@@ -0,0 +1,33 @@
+//Fibonacci Loop
+
+function fibonacci(n) {
+    let result = []
+
+    if (n === 0) {
+        return result
+    } 
+    result.push(0)
+
+    if (n == 1) {
+        return result;
+    }
+    result.push(1)
+
+    if (n == 2) {
+        return result
+    } 
+
+    for(let i = 2; i < n; i++) {
+        let temp = result[i - 1] + result[i - 2]; 
+        result.push(temp); 
+    }
+
+    return result;
+}
+
+
+console.log(fibonacci(0));
+console.log(fibonacci(1)); 
+console.log(fibonacci(2)); 
+console.log(fibonacci(5)); 
+console.log(fibonacci(10)); 
